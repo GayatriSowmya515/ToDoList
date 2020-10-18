25831bb41fc93bae3cdb8d124d572cb63759857e
sowmya <kayaackerman515@gmail.com>
2020-10-18 07:53:47 +0530

    final

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let todoItems = [];
 
-
 function addTodo(text) {
     const todo = {
         text,
@@ -42,18 +41,11 @@ function renderTodo(todo) {
     // select the current todo item in the DOM
     const item = document.getElementById('' + todo.id);
 
-    // add this if block
-    if (todo.deleted) {
-        // remove the item from the DOM
-        console.log('TODO-DELETED!!!!!!!!')
-        item.remove();
-        return
-    }
-
     //const isChecked = todo.checked ? 'done' : '';
 
-    const node = document.createElement('li');
+    const node = document.createElement('div');
     node.setAttribute('id', todo.id);
+    node.setAttribute('class', 'list')
     node.innerHTML = `
           <input type="checkbox" class="checkbox" id="checkbox-${todo.id}" name="${todo.id}">
            <label class="todos-list__li" for="${todo.id}">
@@ -61,7 +53,7 @@ function renderTodo(todo) {
              <div class="todos-list__li-icon">
                 <img src="/img/checkbox.svg" alt="checkbox" class="todos-list__li-icon-1"/>
                
-                   <svg class="todos-list__li-icon-2" width="30" height="25" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                   <svg class="todos-list__li-icon-2" width="28" height="22" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                    <path id="tick" d="M2 14.2807L9.81268 22L28 2" stroke="url(#paint0_linear)" stroke-width="3.2"/>
                    <defs>
                    <linearGradient id="paint0_linear" x1="28" y1="2" x2="-4.65091" y2="11.0317" gradientUnits="userSpaceOnUse">
@@ -74,7 +66,7 @@ function renderTodo(todo) {
                
            <div class="todos-list__li-text">
                 <div class = "todos-list__li-text-1"> ${todo.text}</div>
-                <svg class="todos-list__li-text-2" width="194" height="4" viewBox="0 0 194 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg class="todos-list__li-text-2" width="170" height="4" viewBox="0 0 194 4" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path id="line" d="M0 2L194 2" stroke="url(#paint0_linear)" stroke-width="3.2"/>
                      <defs>
                     <linearGradient id="paint0_linear" x1="194" y1="2.00012" x2="0" y2="2.00003" gradientUnits="userSpaceOnUse">
@@ -89,14 +81,8 @@ function renderTodo(todo) {
            </label>
         `;
     console.log(node);
-
     list.append(node);
-
-
 }
-
-
-
 
 
 //Mark a task as completed
@@ -122,12 +108,10 @@ list.addEventListener('click', event => {
     }
     else if (event.target.classList.contains('todos-list__li-text') ||
         event.target.classList.contains('todos-list__li-icon')) {
-
         const itemKey = event.target.parentNode.parentNode.id;
         toggleDone(itemKey);
         if (event.target.parentNode.parentNode.classList.contains('checked')) {
             document.getElementById("checkbox-" + itemKey).checked = true;
-            console.log("cehceeeeeeeekkkkkkkkkkkeeeeeddddddd" + document.getElementById("checkbox-" + itemKey).checked);
         }
         else {
             event.target.style.textDecoration = "none";
@@ -141,17 +125,11 @@ list.addEventListener('click', event => {
         toggleDone(itemKey);
         if (event.target.parentNode.parentNode.parentNode.classList.contains('checked')) {
             document.getElementById("checkbox-" + itemKey).checked = true;
-            console.log("cehceeeeeeeekkkkkkkkkkkeeeeeddddddd" + document.getElementById("checkbox-" + itemKey).checked);
         }
         else {
-            event.target.style.textDecoration = "none";
             document.getElementById("checkbox-" + itemKey).checked = false;
         }
-
     }
-
-
-
 });
 
 
@@ -168,9 +146,7 @@ function toggleDone(key) {
     todoItems[index].checked = !todoItems[index].checked;
 
     document.getElementById(todoItems[index].id).classList.toggle('checked');
-    // document.querySelector('input[type="checkbox"]').setAttribute('checked', todoItems[index].checked);
-
-    console.log('Checked - 2 : ' + todoItems[index].checked);
+    console.log('Checked : ' + todoItems[index].checked);
 
 }
 
@@ -186,8 +162,12 @@ function deleteTodo(key) {
         deleted: true,
         ...todoItems[index]
     };
+    document.getElementById(key).classList.add('remove');
     // remove the todo item from the array by filtering it out
-    todoItems = todoItems.filter(item => item.id !== Number(key));
+    document.getElementById(key).addEventListener("transitionend", function () {
+        document.getElementById(key).remove();
+        todoItems = todoItems.filter(item => item.id !== Number(key));
+    });
 
-    renderTodo(todo);
+
 }
